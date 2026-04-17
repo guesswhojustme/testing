b99dc5bd2808958a6bd84f95d5c82812234a7b95
Jjae <143724847+guesswhojustme@users.noreply.github.com>
2026-04-17 15:52:28 +0800

    capitalize testing

--- script.js
+++ script.js
@@ -1,4 +1,19 @@
 
-export function sum(a, b){
-    return a + b
+export function capitalize(str){
+    let newString = '';
+    let splitString = str.split('');
+    const capitalFirstLetter = [];
+
+    for(let i = 0; i <= splitString.length - 1; i++){
+        if(i == 0){
+            capitalFirstLetter.push(splitString[i].toUpperCase())
+        }else{
+            capitalFirstLetter.push(splitString[i])
+        }
+    }
+    const convertToString = capitalFirstLetter.join("");
+    console.log(splitString);
+    console.log(capitalFirstLetter);
+    console.log(convertToString);
+    return convertToString;
 }
